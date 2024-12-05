1b4543231d761050c0f9f37829d6de3d5f1c2326
Evgeniy-Sergeev <manalota7@gmail.com>
2024-12-05 17:21:38 +0300

    rfvbn

---
+++ newapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from newapp.apps import NewappConfig
+from newapp.views import contacts
+
+app_name = NewappConfig.name
+
+urlpatterns = [
+    path('contacts/', contacts, name='contacts')
+]
