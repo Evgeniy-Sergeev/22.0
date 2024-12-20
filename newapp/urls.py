83e0dbed32ef3e13d0652399d3093a0b3e4034a8
Evgeniy-Sergeev <manalota7@gmail.com>
2024-12-20 15:47:58 +0300

    try 52

--- newapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from newapp.apps import NewappConfig
-from newapp.views import contacts
-
-app_name = NewappConfig.name
-
-urlpatterns = [
-    path('contacts/', contacts, name='contacts')
-]
